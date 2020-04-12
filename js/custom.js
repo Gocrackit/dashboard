51bfa10fab53cf2732bf109166b12d2b78ad360b
Gocrackit <sudhanshumishra36@gmail.com>
2020-04-12 21:42:57 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+function openForm() {
+  document.getElementById("myForm").style.display="block";
+}
+function findDonor() {
+  document.getElementById("find-donor").style.display="block";
+}
+function closeDonor() {
+  document.getElementById("find-donor").style.display="none";
+}
+function open_login_Form() {
+  document.getElementById("login-form").style.display="block";
+}
+
+function close_login_Form() {
+  document.getElementById("login-form").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+    onclick=document.getElementById("myForm").style.display="none";
+}
